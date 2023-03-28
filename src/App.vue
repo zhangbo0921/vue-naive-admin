62f7feea9b51dc9d693b1ab5413ee2ce6d7ccb17
zhangbo <15686056663@163.com>
2023-03-28 17:55:36 +0800

    中文

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { zhCN, dateZhCN } from 'naive-ui'
+</script>
 <template>
-  <n-button>Hello NaiveUI</n-button>
+  <n-config-provider :locale="zhCN" :date-locale="dateZhCN">
+    <n-button>Hello NaiveUI</n-button>
+    <n-input></n-input>
+    <n-date-picker type="date" />
+  </n-config-provider>
 </template>
